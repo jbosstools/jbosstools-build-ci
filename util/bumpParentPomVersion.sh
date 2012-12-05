9ebddb9299f12e8066f8987521a26e70b7595cb8
nickboldt <nickboldt@gmail.com>
2012-12-05 11:50:49 -0500

    bump example to latest released version

--- util/bumpParentPomVersion.sh
+++ util/bumpParentPomVersion.sh
@@ -4,6 +4,6 @@ new=$2
 if [[ $1 ]] && [[ $2 ]]; then
 	for p in $(find.sh . pom\*.xml ${old} target "" -q); do sed -i -e "s#<version>${old}</version>#<version>${new}</version>#g" $p; done; 
 else
-	echo "Usage: $0 4.0.0.M1-SNAPSHOT 4.0.0.Beta1-SNAPSHOT"; exit 1
+	echo "Usage: $0 4.0.0.CR1-SNAPSHOT 4.0.0.Final-SNAPSHOT"; exit 1
 fi
 
