d1c4e5043b95d5b5d7328ae945e75b74d52a4759
nickboldt <nickboldt@gmail.com>
2014-06-03 14:30:42 -0400

    JBIDE-17519 ensure script works for JBT/JBDS, JBT Central/EA, and JBTIS too

---
+++ util/verifyTarget.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+# commandline args:
+
+# point BASEDIR to where you have jbosstools-discovery, jbosstools-target-platforms, or jbosstools-integration-stack sources checked out
+# ideally, you would run this script from that folder, or set 
+# -b /path/to/jbosstools-discovery
+# If running in the current directory, use 
+# -b `pwd`
+
+# comma-separated list of projects to build
+# -p jbtcentral,jbearlyaccess
+# -p jbosstools,jbdevstudio
+
+# OPTIONAL if you want to perform an install test, rather than just ensure that your TP can be validated and resolved locally
+# set path to where you have the latest compatible Eclipse bundle stored locally
+# -z /path/to/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz
+
+# OPTIONAL if you're testing a TP that's downstream from the jbosstoolstarget or jbdevstudiotarget, eg., Central, Early Access, or Integration Stack
+# set URL(s) for JBT / JBT Target so that all Central deps can be resolved; for more than one, separate w/ commas
+# -u file://$HOME/tru/jbosstools-target-platforms/jbosstools/multiple/target/jbosstools-multiple.target.repo/,http://download.jboss.org/jbosstools/updates/nightly/core/master/
+# for JBDS Central tests, use:
+# -u file://$HOME/tru/jbosstools-target-platforms/jbdevstudio/multiple/target/jbdevstudio-multiple.target.repo/,http://www.qa.jboss.com/binaries/RHDS/builds/staging/devstudio.product_master/all/repo/
+
+# OPTIONAL if you want to perform a p2diff between your last version of the TP and the one you're about to build
+# set path to where you have the latest p2diff executable installed
+# -d ${HOME}/tmp/p2diff/p2diff
+
+usage ()
+{
+  echo "Usage: $0 -b BASEDIR -p PROJECT1,PROJECT2,... [-z ECLIPSEZIP] [-u UPSTREAM_SITES] [-d P2DIFF]"
+  echo ""
+  echo "Example: $0 -b /path/to/jbosstools-target-platforms -p jbosstools,jbdevstudio \\"
+  echo "  -z /path/to/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz -d /path/to/executable/p2diff"
+  echo ""
+  echo "Example: $0 -b /path/to/jbosstools-discovery -p jbtcentral,jbearlyaccess \\"
+  echo "  -z /path/to/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz  -d /path/to/executable/p2diff \\"
+  echo "  -u http://download.jboss.org/jbosstools/targetplatforms/jbosstoolstarget/4.40.0.Beta4-SNAPSHOT/REPO/,http://download.jboss.org/jbosstools/updates/nightly/core/master/"
+  echo ""
+  echo "Example: $0 -b /path/to/jbosstools-discovery -p jbtcentral,jbearlyaccess \\"
+  echo "  -z /path/to/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz  -d /path/to/executable/p2diff \\"
+  echo "  -u file://path/to/jbosstools-target-platforms/jbosstools/multiple/target/jbosstools-multiple.target.repo/,file://path/to/jbosstools-build-sites/aggregate/site/target/"
+  echo ""
+  echo "Example: $0 -b /path/to/jbosstools-integration-stack -p target-platform \\"
+  echo "  -z /path/to/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz -d /path/to/executable/p2diff"
+  echo ""
+  exit 1;
+}
+
+if [[ $# -lt 1 ]]; then
+  usage;
+fi
+
+# defaults
+targetplatformutilsversion=0.19.0-SNAPSHOT
+#BASEDIR=`pwd`
+#ECLIPSEZIP=${HOME}/tmp/Eclipse_Bundles/eclipse-jee-luna-M7-linux-gtk-x86_64.tar.gz
+#UPSTREAM_SITES=file://$HOME/tru/jbosstools-target-platforms/jbosstools/multiple/target/jbosstools-multiple.target.repo/,http://download.jboss.org/jbosstools/updates/nightly/core/master/
+# for JBDS tests, use UPSTREAM_SITES=file://$HOME/tru/jbosstools-target-platforms/jbdevstudio/multiple/target/jbdevstudio-multiple.target.repo/,http://www.qa.jboss.com/binaries/RHDS/builds/staging/devstudio.product_master/all/repo/
+#P2DIFF=${HOME}/tmp/p2diff/p2diff
+
+# read commandline args
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-b') BASEDIR="$2"; shift 1;;
+    '-z') ECLIPSEZIP="$2"; shift 1;;
+    '-u') UPSTREAM_SITES="$2"; shift 1;;
+    '-d') P2DIFF="$2"; shift 1;;
+    '-p') PROJECTS="$PROJECTS $2"; shift 1;;
+       *) others="$others,$1"; shift 0;;
+  esac
+  shift 1
+done
+
+if [[ ! $BASEDIR ]]; then
+  echo ""; echo "Error: BASEDIR not set; to run in current directory, use -p "'`pwd`'; echo ""; usage;
+fi
+if [[ ! -d $BASEDIR ]]; then
+  echo ""; echo "Error: BASEDIR is not a directory; to run in current directory, use -p "'`pwd`'; echo ""; usage;
+fi
+if [[ ! $PROJECTS ]]; then
+  echo ""; echo "Error: PROJECTS not set!"; echo ""; usage;
+fi
+
+# replace commas for spaces so we can for-loop through the projects
+PROJECTS=${PROJECTS//,/ }
+NOW=`date +%F_%H-%M`
+
+for PROJECT in $PROJECTS; do echo "Process $PROJECT ..."
+
+  if [[ -d ${BASEDIR}/${PROJECT}target ]]; then 
+    WORKSPACE=${BASEDIR}/${PROJECT}target
+  elif [[ -d ${BASEDIR}/${PROJECT} ]]; then 
+    WORKSPACE=${BASEDIR}/${PROJECT}
+  else 
+    WORKSPACE=${BASEDIR}
+  fi
+
+  # JBoss Tools uses /multiple/ folders and multiple2repo profile
+  # Integration Stack uses /target-platform/ folder and isbtp2repo profile
+  # so check which convention to use
+  if [[ -d ${WORKSPACE}/target-platform ]]; then
+    WORKDIR=${WORKSPACE}/target-platform
+    REPODIR=target-platform.target.repo
+    PROFILE=isbtp2repo
+  elif [[ -d ${WORKSPACE}/../target-platform ]]; then
+    WORKDIR=${WORKSPACE}
+    REPODIR=target-platform.target.repo
+    PROFILE=isbtp2repo
+  elif [[ -d ${WORKSPACE}/multiple ]]; then
+    WORKDIR=${WORKSPACE}/multiple
+    REPODIR=${PROJECT}-multiple.target.repo
+    PROFILE=multiple2repo
+  fi
+
+  if [[ ! -d ${WORKDIR} ]]; then
+    echo "Error: cannot find WORKDIR = ${WORKDIR} - must exit."
+    exit 1
+  fi
+
+  # TODO: remember to clean these out from /tmp
+  if [[ -d ${WORKDIR}/target/${REPODIR}/ ]] && [[ ${P2DIFF} ]] && [[ -x ${P2DIFF} ]]; then
+    echo ""
+    echo "Step 0: To prepare to p2diff your last target platform against the one you're about to build,"
+    echo "        backup (move) the existing target platform folder to /tmp/${REPODIR}_${NOW} ..."
+    echo ""
+    mv ${WORKDIR}/target/${REPODIR}/ /tmp/${REPODIR}_${NOW} && touch /tmp/${REPODIR}_${NOW}
+  fi
+
+  echo ""
+  echo "Step 1: Merge changes in new target file produce corrected/updated target file,"
+  echo "        replacing any 0.0.0 or obsolete versions with latest versions ..."
+  echo ""
+  pushd ${WORKDIR}
+
+  for tf in *.target; do 
+    mvn -U org.jboss.tools.tycho-plugins:target-platform-utils:${targetplatformutilsversion}:fix-versions -DtargetFile=${tf} \
+      | tee /tmp/fix-versions_${tf}_log_${PROJECT}_${NOW}.txt; if [[ "$?" != "0" ]]; then exit; fi
+    if [[ -f ${tf}_fixedVersion.target ]]; then rm -f ${tf} *_update_hints.txt; mv -f ${tf}{_fixedVersion.target,}; fi
+  done
+  popd
+
+  echo ""
+  echo "Step 2: Resolve target platform (including sources). This may take"
+  echo "        more than an hour depending on network performance ... "
+  echo ""
+
+  # TODO: if you removed IUs, be sure to do a `mvn clean install`, rather than just a `mvn install`; process will be much longer but will guarantee metadata is correct
+  pushd ${WORKSPACE}
+  mvn install -P${PROFILE} -DtargetRepositoryUrl=file://${WORKDIR}/target/${REPODIR}/ -Dmirror-target-to-repo.includeSources=true | tee /tmp/resolve_log_${PROJECT}_${NOW}.txt; if [[ "$?" != "0" ]]; then exit; fi
+  popd
+
+  if [[ -f ${ECLIPSEZIP} ]]; then 
+    echo ""
+    echo "Step 3: Install the new target platform into a clean Eclipse JEE bundle"
+    echo "        to verify that everything can be installed ..."
+    echo ""
+    INSTALLDIR=/tmp/${PROJECT}target-install-test
+    INSTALLSCRIPT=/tmp/installFromTarget.sh
+    rm -fr ${INSTALLDIR} && mkdir -p ${INSTALLDIR}
+    pushd ${INSTALLDIR}
+      echo "  Unpack ${ECLIPSEZIP} into ${INSTALLDIR} ..."
+      tar xzf ${ECLIPSEZIP}
+      echo "  Fetch target install script to ${INSTALLSCRIPT} ..." 
+      wget -q --no-check-certificate -N https://raw.githubusercontent.com/jbosstools/jbosstools-build-ci/master/util/installFromTarget.sh -O ${INSTALLSCRIPT}
+      chmod +x ${INSTALLSCRIPT}
+      echo "  Install..."
+      if [[ ${UPSTREAM_SITES} ]]; then
+        ${INSTALLSCRIPT} -ECLIPSE ${INSTALLDIR}/eclipse -INSTALL_PLAN ${UPSTREAM_SITES},file://${WORKDIR}/target/${REPODIR}/ | tee ${INSTALLSCRIPT}_log_${PROJECT}_${NOW}.txt
+      else
+        echo ""
+        echo "  No UPSTREAM_SITES specified. If installation fails, try adding more upstream sites to help resolving dependencies."
+        echo ""
+        ${INSTALLSCRIPT} -ECLIPSE ${INSTALLDIR}/eclipse -INSTALL_PLAN file://${WORKDIR}/target/${REPODIR}/ | tee ${INSTALLSCRIPT}_log_${PROJECT}_${NOW}.txt
+      fi
+      echo ""
+      echo "  Scan log ( ${INSTALLSCRIPT}_log_${PROJECT}_${NOW}.txt ) for errors ..."
+      echo ""
+      cat ${INSTALLSCRIPT}_log_${PROJECT}_${NOW}.txt | egrep -i -A2 "IllegalArgumentException|Could not resolve|error|Unresolved requirement|could not be found|FAILED|Missing|Only one of the following|being installed|Cannot satisfy dependency"; if [[ "$?" == "0" ]]; then break; fi
+    popd
+  else
+    echo ""
+    echo "Step 3: no ECLIPSEZIP specified, so cannot perform installation test."
+    echo ""
+  fi
+
+  if [[ ${P2DIFF} ]] && [[ -x ${P2DIFF} ]]; then
+    echo ""
+    echo "Step 4: produce p2diff report ..."
+    echo ""
+    ${P2DIFF} /tmp/${REPODIR}_${NOW} file://${WORKDIR}/target/${REPODIR}/ | tee /tmp/p2diff_log_${PROJECT}_${NOW}.txt
+  elif [[ ${P2DIFF} ]]; then
+    echo ""
+    echo "Step 4: cannot execute p2diff from ${P2DIFF} - nothing to do."
+  else
+    echo ""
+    echo "Step 4: no P2DIFF specified, so cannot perform p2diff."
+  fi
+
+done
+
+echo ""
+echo "Logs & temporary files"
+echo ""
+for d in /tmp/${REPODIR}_${NOW} /tmp/fix-versions_log_${PROJECT}_${NOW}.txt /tmp/resolve_log_${PROJECT}_${NOW}.txt ${INSTALLSCRIPT}_log_${PROJECT}_${NOW}.txt /tmp/p2diff_log_${PROJECT}_${NOW}.txt; do
+  if [[ -f $d ]]; then
+    echo "F: $d"
+  elif [[ -d $d ]]; then
+    echo "D: $d"
+  else
+    echo "?: $d"
+  fi
+done
