c3087121468c0c48f8035f9de7765351126b4f72
nickboldt <nickboldt@gmail.com>
2014-08-29 18:58:04 -0400

    add discovery to the list of places to check

--- util/getProjectSHAs.sh
+++ util/getProjectSHAs.sh
@@ -3,7 +3,7 @@
 stream=4.2.luna
 branch=4.2.x
 
-for j in aerogear arqullian base birt browsersim central forge freemarker hibernate javaee jst livereload openshift portlet server vpe webservices; do
+for j in aerogear arquillian base birt browsersim central discovery forge freemarker hibernate javaee jst livereload openshift portlet server vpe webservices; do
   echo "== ${j} =="
   githash=`wget -q https://api.github.com/repos/jbosstools/jbosstools-${j}/commits/jbosstools-${branch} -O - | head -2 | grep sha | \
   	sed "s#  \"sha\": \"\(.\+\)\",#\1 (${branch})#"`
