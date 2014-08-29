cd5d67661307cc2a9a082ef7d91a51688382d8eb
nickboldt <nickboldt@gmail.com>
2014-08-29 18:12:57 -0400

    JBIDE-17932 check more projects and enable validation for missing branches

--- util/getProjectSHAs.sh
+++ util/getProjectSHAs.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
 stream=4.2.luna
-branch=4.2.0.Beta3x
+branch=4.2.x
 
-for j in aerogear birt central forge server hibernate javaee jst openshift vpe webservices; do
+for j in aerogear arqullian base birt browsersim central forge freemarker hibernate javaee jst livereload openshift portlet server vpe webservices; do
   echo "== ${j} =="
   githash=`wget -q https://api.github.com/repos/jbosstools/jbosstools-${j}/commits/jbosstools-${branch} -O - | head -2 | grep sha | \
   	sed "s#  \"sha\": \"\(.\+\)\",#\1 (${branch})#"`
   jenkinshash=`wget -q http://download.jboss.org/jbosstools/builds/staging/jbosstools-${j}_${stream}/logs/GIT_REVISION.txt -O - | grep jbosstools | \
   	sed "s#\(.\+\)\@\(.\+\)#\2 (${stream}, \1)#"`
-  if [[ ${githash%% *} == ${jenkinshash%% *} ]]; then # match
+  if [[ ! ${jenkinshash} ]]; then
+    echo "ERROR: branch $branch does not exist" | egrep ERROR
+  elif [[ ${githash%% *} == ${jenkinshash%% *} ]]; then # match
   	echo "PASS: ${jenkinshash}"
   else
   	echo "FAIL:" | grep FAIL
   	echo "      $jenkinshash"
   	echo "      $githash"
+  fi
+  echo ""
+done
+
+stream=8.0.luna
+for j in product; do
+  echo "== ${j} =="
+  githash=`wget -q https://api.github.com/repos/jbdevstudio/jbdevstudio-${j}/commits/jbosstools-${branch} -O - | head -2 | grep sha | \
+    sed "s#  \"sha\": \"\(.\+\)\",#\1 (${branch})#"`
+  jenkinshash=`wget -q http://www.qa.jboss.com/binaries/RHDS/builds/staging/devstudio.${j}_${stream}/logs/GIT_REVISION.txt -O - | grep jbosstools | \
+    sed "s#\(.\+\)\@\(.\+\)#\2 (${stream}, \1)#"`
+  if [[ ! ${jenkinshash} ]]; then
+    echo "ERROR: branch $branch does not exist" | egrep ERROR
+  elif [[ ${githash%% *} == ${jenkinshash%% *} ]]; then # match
+    echo "PASS: ${jenkinshash}"
+  else
+    echo "FAIL:" | grep FAIL
+    echo "      $jenkinshash"
+    echo "      $githash"
   fi
   echo ""
 done
