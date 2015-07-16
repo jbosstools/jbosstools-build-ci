2bbc14fce1f2d8eb645ab4b3da0c4af3e4377c06
nickboldt <nickboldt@gmail.com>
2015-07-16 09:33:54 -0400

    make checkAlternatives optional with -s flag; improve reporting w/ colour, counter, and summary

--- util/checkIfBranchesExist.sh
+++ util/checkIfBranchesExist.sh
@@ -8,6 +8,7 @@ usage ()
 	echo "Example: $0 -b jbosstools-4.3.0.Alpha1x aerogear arquillian base birt browsersim central forge \\"
 	echo " freemarker hibernate javaee jst livereload openshift portlet server vpe webservices discovery \\"
 	echo " build build-ci build-sites download.jboss.org"
+	echo "Use -s to report similar branches (eg., for 4.3.0.Beta2x, search for *Beta2x)"
 	exit 1;
 }
 
@@ -16,12 +17,13 @@ if [[ $# -lt 1 ]]; then
 fi
 
 # projects to check if branched
-#projects="base birt build build-ci build-sites central download.jboss.org forge freemarker gwt hibernate intergration-tests javaee jst maven-plugins openshift portlet server target-platforms vpe webservices"
+#projects="base birt build build-ci build-sites central download.jboss.org forge freemarker hibernate intergration-tests javaee jst maven-plugins openshift playground portlet server target-platforms vpe webservices"
 
 # read commandline args
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		'-b') branch="$2"; shift 1;;
+		'-s') checkAlternatives=1; shift;;
 		*) projects="$projects $1";;
 	esac
 	shift 1
@@ -38,25 +40,39 @@ debug ()
 {
 	if [[ $debug -gt 0 ]]; then echo "$1"; fi
 }
+
+norm="\033[0;39m"
+green="\033[1;32m"
+red="\033[1;31m"
+
 # for branch = jbosstools-4.3.0.Alpha1x  check also for a branch ending in Alpha1x
 branchAlt=${branch##*.}; 
 cd /tmp
+cnt=0
+OK=0
 for d in $projects; do 
-	echo -n "$d"
+  (( cnt++ ))
+	echo -n -e "[${cnt}] ${norm}$d"
 	if [[ `wget https://github.com/jbosstools/jbosstools-${d}/tree/${branch} 2>&1 | egrep "ERROR 404"` ]]; then
-		echo ""
 		#echo " * Not found  checking https://api.github.com/repos/jbosstools/jbosstools-${d}/branches for branches ending in ${branchAlt} ..."
-		altBranches=`wget https://api.github.com/repos/jbosstools/jbosstools-${d}/branches 2>/dev/null -O - | egrep "\"name\":" | egrep "${branchAlt}"`
-		if [[ $altBranches ]]; then
-			echo "* Found possible alternate branches:" 
-			echo "$altBranches"
+		if [[ $checkAlternatives ]]; then
+			echo " ... "
+			altBranches=`wget https://api.github.com/repos/jbosstools/jbosstools-${d}/branches 2>/dev/null -O - | egrep "\"name\":" | egrep "${branchAlt}"`
+			if [[ $altBranches ]]; then
+				echo "* Found possible alternate branches:" 
+				echo "$altBranches"
+			else
+				debug "* Branch $branch not found; no alternates found at https://api.github.com/repos/jbosstools/jbosstools-${d}/branches:"
+				debug "$altBranches"
+			fi
 		else
-			debug "* Branch $branch not found; no alternates found at https://api.github.com/repos/jbosstools/jbosstools-${d}/branches:"
-			debug "$altBranches"
+			echo -e "... ${red}NO${norm}"
 		fi
 	else
-		echo " ... OK"
+		echo -e " ... ${green}OK${norm}"
+		(( OK++ ))
 	fi
 	rm -f ${branch} branches
 	echo ""
 done
+echo -e "${norm}OK: ${green}$OK${norm} of $cnt"
