a0eb9a7716cfa7d0c81365899227a55ead3cfb7a
nickboldt <nickboldt@gmail.com>
2015-07-30 14:46:30 -0400

    JBIDE-16158 clean up and rename README; add usage instructions and better output for etc/todirformat.sh; add more to .gitignore

--- buildchow/etc/todirformat.sh
+++ buildchow/etc/todirformat.sh
@@ -1,14 +1,38 @@
-FILES=$1/*_$2
+#!/bin/bash
 
-TOPATH=$1-old/$2/job
-echo Making $TOPATH
-mkdir -p $TOPATH
+# this script will take a folder full of config files called ../configs/default/generated/jbosstools-base_4.3.mars
+# and rename them into ../configs/default/generated-old/master/job/jbosstools-base_4.3.mars/config.xml
 
-for f in $FILES
-do
-  echo "Processing $f file..."
+usage ()
+{
+    echo "Usage:     $0 /path/to/source/folder site_stream"
+    echo "Example 1: $0 ../configs/default/generated 4.3.mars"
+    exit 1;
+}
+
+if [[ $# -lt 1 ]]; then
+  usage;
+fi
+SRCDIR=${1%/} # trim trailing slash if present
+STREAM=${2}
+FILES=${SRCDIR}/*_${STREAM}
+
+DESTDIR=${SRCDIR}-old/${STREAM}/job
+mkdir -p $DESTDIR
+
+cnt=0
+for f in $FILES; do
   # take action on each file. $f store current file name
-  DEST=$TOPATH/$(basename $f)
-  mkdir $DEST
-  cp $f $DEST/config.xml
+  DEST=$DESTDIR/$(basename $f)
+  if [[ -d $DEST ]]; then 
+  	echo "[WARNING] Directory already exists: $DEST"
+  else
+	  # echo "[INFO] [$cnt] Processing $f ..."
+  	mkdir -p $DEST
+  	cp $f $DEST/config.xml
+	  (( cnt++ ))
+  fi
 done
+
+echo ""
+echo "[INFO] $cnt config.xml files produced in $DESTDIR" 
