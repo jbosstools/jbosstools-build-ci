4a37b13589a4c237bcc9bc9ff5946f31f090519e
nickboldt <nickboldt@gmail.com>
2016-04-18 12:04:40 -0400

    to support publishing from mars/staging to static/mars/development, need a SRC_DIR var; also clean up doc/examples and remove -eclipseReleaseName / -devstudioReleaseVersion flags

--- publish/stage.sh
+++ publish/stage.sh
@@ -29,13 +29,29 @@ log ()
 # can be used to publish a build (including installers, site zips, MD5s, build log) or just an update site folder
 usage ()
 {
-  echo "Usage  : $0 [-DESTINATION destination] -s source_path -t target_path"
+  echo "Usage  : $0 -sites \"site1 site2 ...\" -stream STREAM -vr versionWithRespin -sd SRC_DIR [-dd DESTDIR] [-st SRC_TYPE] [-dt DESTTYPE] \\"
+  echo "            [-JOB_NAME JOB_NAME] [-DESTINATION user@server:/path] [-DEST_URL http://server.url] [-WORKSPACE WORKSPACE] [-ID ID] [-q]"
   echo ""
 
-  echo "To stage JBT core, coretests, central & earlyaccess"
-  echo "   $0 -sites \"site coretests-site central-site earlyaccess-site\" -dd mars -stream 4.3.mars -vr 4.3.1.CR1a -JOB_NAME jbosstools-build-sites.aggregate.\\\${site}_\\\${stream}"
-  echo "To stage JBT discovery.* sites & browsersim-standalone"
-  echo "   $0 -sites \"discovery.central discovery.earlyaccess browsersim-standalone\" -dd mars -stream 4.3.mars -vr 4.3.1.CR1a -q"
+  echo "To stage JBT core, coretests, central & earlyaccess (4 builds)"
+  echo "   $0 -sites \"site coretests-site central-site earlyaccess-site\" -stream 4.3.mars -vr 4.3.1.CR1a -sd mars -JOB_NAME jbosstools-build-sites.aggregate.\\\${site}_\\\${stream}"
+  echo "To stage JBT discovery.* sites & browsersim-standalone (3 builds)"
+  echo "   $0 -sites \"discovery.central discovery.earlyaccess browsersim-standalone\" -stream 4.3.mars -vr 4.3.1.CR1a -sd mars -q"
+  echo ""
+
+  echo "To stage JBDS product (1 build)"
+  echo "   $0 -sites product -stream 9.0.mars -vr 9.1.0.CR1c -sd 9.0 -JOB_NAME devstudio.\\\${site}_\\\${stream} \\"
+  echo "      -DESTINATION \${JBDS} -DEST_URL https://devstudio.redhat.com -q &"
+  echo "To stage JBDS discovery sites (4 builds)"
+  echo "   $0 -sites \"central-site earlyaccess-site discovery.central discovery.earlyaccess\" -stream 4.3.mars -vr 9.1.0.CR1c -sd 9.0 -JOB_NAME jbosstools-\\\${site}_\\\${stream} \\"
+  echo "      -DESTINATION \${JBDS} -DEST_URL https://devstudio.redhat.com -q"
+  echo "(NOTE: must also publish target platform zips; that step is not yet covered here.)"
+  echo ""
+
+  echo "To release a JBT milestone (7 builds)"
+  echo "   for site in site coretests-site central-site earlyaccess-site discovery.central discovery.earlyaccess browsersim-standalone; do \\"
+  echo "     $0 -sites \$site -stream 4.3.1.CR1c -vr 4.3.1.Final -sd mars -dd static/mars -st staging -dt development -JOB_NAME jbosstools-\\\${stream}-build-\\\${site}; \\"
+  echo "   done"
   exit 1
 }
 
@@ -46,14 +62,15 @@ while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-sites') sites="${sites} $2"; shift 1;; # site coretests-site central-site earlyaccess-site discovery.central discovery.earlyaccess
   
-    '-eclipseReleaseName') DESTDIR="$2"; shift 1;; # mars or neon
-    '-devstudioReleaseVersion') DESTDIR="$2"; shift 1;; # 9.0 or 10.0
-    '-DESTDIR'|'-dd') DESTDIR="$2"; shift 1;; # mars or 9.0 or neon or 10.0
+    '-stream') stream="$2"; shift 1;; # for staging, use 4.3.mars or 9.0.mars; for release, use the versionWithRespin value, eg., 4.3.1.CR1c or 9.1.0.CR1c
+    '-versionWithRespin'|'-vr') versionWithRespin="$2"; shift 1;; # for staging, use 4.3.1.CR1c, 9.1.0.CR1c; for release, use 4.3.1.Final or 9.1.0.GA
 
-    '-SRC_TYPE') SRC_TYPE="$2"; shift 1;; # by default, snapshots but could be staging? (TODO: UNTESTED!)
-    '-DESTTYPE') DESTTYPE="$2"; shift 1;; # by default, staging but could be development? (TODO: UNTESTED!)
-    '-stream') stream="$2"; shift 1;; # 4.3.mars (TODO: could be 9.0.mars?)
-    '-versionWithRespin'|'-vr') versionWithRespin="$2"; shift 1;; # 4.3.0.CR2b, 9.1.0.CR2b
+    '-SRC_DIR'|'-sd') SRC_DIR="$2"; shift 1;; # mars or 9.0 or neon or 10.0 (if not set, default to same value as DESTDIR)
+    '-DESTDIR'|'-dd') DESTDIR="$2"; shift 1;; # mars or 9.0 or neon or 10.0, or could be static/mars, static/9.0, etc.
+
+    '-SRC_TYPE'|'-st') SRC_TYPE="$2"; shift 1;; # by default, snapshots but could be staging? (TODO: UNTESTED!)
+    '-DESTTYPE'|'-dt') DESTTYPE="$2"; shift 1;; # by default, staging but could be development? (TODO: UNTESTED!)
+
     '-DJOB_NAME'|'-JOB_NAME') rawJOB_NAME="$2"; shift 1;;
 
     '-DESTINATION') DESTINATION="$2"; shift 1;; # override for JBDS publishing, eg., devstudio@filemgmt.jboss.org:/www_htdocs/devstudio or /qa/services/http/binaries/RHDS
@@ -69,8 +86,12 @@ done
 
 if [[ $quiet == 1 ]]; then consoleDest=/dev/null; else consoleDest=/dev/tty; fi
 
-# set mars/staging, 9.0/staging, etc.
-if [[ ! ${DESTDIR} ]]; then echo "ERROR: DESTDIR not set. Please define eclipseReleaseName or devstudioReleaseVersion"; echo ""; usage; fi
+# set mars, 9.0, etc.
+if [[ ! ${DESTDIR} ]] && [[ ! ${SRC_DIR} ]]; then echo "ERROR: DESTDIR and SRC_DIR not set. Please set at least one."; echo ""; usage; fi
+
+# if one set and not the other, set DESTDIR and SRC_DIR equal to each other
+if [[ ! ${DESTDIR} ]] && [[ ${SRC_DIR} ]]; then DESTDIR="${SRC_DIR}"; fi
+if [[ ! ${SRC_DIR} ]] && [[ ${DESTDIR} ]]; then SRC_DIR="${DESTDIR}"; fi
 
 if [[ ${DESTINATION/devstudio/} != ${DESTINATION} ]] || [[ ${DESTINATION/RHDS/} != ${DESTINATION} ]]; then
   PRODUCT="devstudio"
@@ -85,16 +106,16 @@ for site in ${sites}; do
 
   if [[ ${whichID} == "latest" ]]; then
     ID=""
-    log "[DEBUG] [$site] + Check ${DEST_URL}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME}" | egrep "${grepstring}"
+    log "[DEBUG] [$site] + Check ${DEST_URL}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME}" | egrep "${grepstring}"
     if [[ ${DESTINATION/@/} == ${DESTINATION} ]]; then # local
-      ID=$(ls ${DESTINATION}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME} | grep "20.\+" | grep -v sftp | sort | tail -1)
+      ID=$(ls ${DESTINATION}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME} | grep "20.\+" | grep -v sftp | sort | tail -1)
     else # remote
-      ID=$(echo "ls 20*" | sftp ${DESTINATION}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME} 2>&1 | grep "20.\+" | grep -v sftp | sort | tail -1)
+      ID=$(echo "ls 20*" | sftp ${DESTINATION}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME} 2>&1 | grep "20.\+" | grep -v sftp | sort | tail -1)
     fi
     ID=${ID%%/*}
-    echo "[INFO] [$site] In ${DESTINATION}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME} found ID = $ID" | egrep "${JOB_NAME}|${site}|${ID}|ERROR"
+    echo "[INFO] [$site] In ${DESTINATION}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME} found ID = $ID" | egrep "${JOB_NAME}|${site}|${ID}|ERROR"
   fi
-  grepstring="${JOB_NAME}|${site}|${ID}|ERROR|${versionWithRespin}|${DESTDIR}|${DESTTYPE}"
+  grepstring="${JOB_NAME}|${site}|${ID}|ERROR|${versionWithRespin}|${SRC_DIR}|${DESTDIR}|${SRC_TYPE}|${DESTTYPE}"
   DEST_URLs=""
 
   RSYNC="rsync -arz --rsh=ssh --protocol=28"
@@ -104,8 +125,8 @@ for site in ${sites}; do
     log "[DEBUG] [$site] Latest build for ${sitename} (${site}): ${ID}" | egrep "${grepstring}"
     # use ${HOME}/temp-stage/ instead of /tmp because insufficient space
     tmpdir=`mkdir -p ${HOME}/temp-stage/ && mktemp -d -t -p ${HOME}/temp-stage/` && mkdir -p $tmpdir && pushd $tmpdir >/dev/null
-      # echo "+ ${RSYNC} ${DESTINATION}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME}/${ID}/* ${tmpdir}/" | egrep "${grepstring}"
-      ${RSYNC} ${DESTINATION}/${DESTDIR}/${SRC_TYPE}/builds/${JOB_NAME}/${ID}/* ${tmpdir}/
+      # echo "+ ${RSYNC} ${DESTINATION}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME}/${ID}/* ${tmpdir}/" | egrep "${grepstring}"
+      ${RSYNC} ${DESTINATION}/${SRC_DIR}/${SRC_TYPE}/builds/${JOB_NAME}/${ID}/* ${tmpdir}/
       # copy build folder
       if [[ ${DESTINATION/@/} == ${DESTINATION} ]]; then # local 
         log "[DEBUG] [$site] + mkdir -p ${DESTINATION}/${DESTDIR}/${DESTTYPE}/builds/${PRODUCT}-${versionWithRespin}-build-${buildname}" | egrep "${grepstring}"
