8627364a1551094630e76d5ce4c8fe7426377b06
nickboldt <nickboldt@gmail.com>
2016-06-08 15:59:51 -0400

    add changeTargetURLs.sh into releng tools, and make it more Jenkins-friendly
    add changeTargetURLs.sh into releng tools, and make it more Jenkins-friendly
    
    JBIDE-22305 make script more jenkins-friendly and add commandline params

--- util/changeTargetURLs.sh
+++ util/changeTargetURLs.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# 0. run in ~/tru/jbosstools-target-platforms
+# 0. run in ~/tru/jbosstools-target-platforms or jenkins workspace with jbosstools-target-platforms checked out to $WORKSPACE
 
-# could be lastBuild, lastCompletedBuild, lastSuccessfulBuild, or a build by number, eg., 38
+usage ()
+{
+  echo "Usage  : $0 -tp TARGET_PLATFORM_VERSION_MAXIMUM [-p2diff /path/to/p2diff] [-b whichBuild]"
+  echo ""
+  echo "Example: $0 -tp 4.60.0.Final-SNAPSHOT -p2diff /home/nboldt/bin/p2diff -b lastSuccessfulBuild"
+  echo "Example: $0 -tp 4.60.0.Final -b 47"
+  echo ""
+  exit 1
+}
+
+if [[ $# -lt 1 ]]; then usage; fi
+
+p2diff=/home/hudson/static_build_env/jbds/p2diff/x86$(if [[ $(uname -a | grep x86_64) ]]; then echo _64; fi)/p2diff
 whichBuild=lastSuccessfulBuild
-if [[ $1 ]]; then whichBuild="$1"; fi 
+
+# read commandline args
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-tp') TARGET_PLATFORM_VERSION_MAXIMUM="$2"; shift 1;; 
+	'-p2diff') p2diff="$2"; shift 1;; # /path/to/p2diff executable
+	'-b') whichBuild="$2"; shift 1;; # could be lastBuild, lastCompletedBuild, lastSuccessfulBuild, or a build by number, eg., 38
+	'-WORKSPACE') WORKSPACE="$2"; shift 1;;
+    *) OTHER="${OTHER} $1"; shift 0;;
+  esac
+  shift 1
+done
 
 if [[ ! ${WORKSPACE} ]]; then WORKSPACE=/tmp; fi
 
@@ -35,20 +58,18 @@ done
 wait
 
 # 4. generate p2diffs
-TPVERSION=4.60.0.Final-SNAPSHOT
-P2DIFF=/home/nboldt/bin/p2diff
 for d in jbosstools jbdevstudio; do
   prefix=http://download.jboss.org/jbosstools; if [[ $d == "jbdevstudio" ]]; then prefix="https://devstudio.jboss.com"; fi
-  p2diffcmd="${P2DIFF} ${prefix}/targetplatforms/${d}target/${TPVERSION}/REPO/ file://"$(pwd)"/${d}/multiple/target/${d}-multiple.target.repo/"
-  ${p2diffcmd} | tee ${WORKSPACE}/p2diff_${d}_${TPVERSION}_latest.txt
+  p2diffcmd="${P2DIFF} ${prefix}/targetplatforms/${d}target/${TARGET_PLATFORM_VERSION_MAXIMUM}/REPO/ file://"$(pwd)"/${d}/multiple/target/${d}-multiple.target.repo/"
+  ${p2diffcmd} | tee ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt
   if [[ ${0/changeTargetURLs.sh/} != $0 ]]; then P2DIFFCHECK=${0/changeTargetURLs.sh/p2diff-check.sh}; else P2DIFFCHECK=~/tru/buildci/util/p2diff-check.sh; fi
-  ${P2DIFFCHECK} ${WORKSPACE}/p2diff_${d}_${TPVERSION}_latest.txt | tee ${WORKSPACE}/p2diff_${d}_${TPVERSION}_summary_latest.txt
+  ${P2DIFFCHECK} ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt | tee ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_summary_latest.txt
 done
 
 echo ""
 for d in jbosstools jbdevstudio; do
-  echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TPVERSION}_latest.txt"
-  echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TPVERSION}_summary_latest.txt"
+  echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt"
+  echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_summary_latest.txt"
 done
 echo ""
 
@@ -58,3 +79,4 @@ git diff --color=never > ${WORKSPACE}/git.diff.txt
 # 6. cleanup
 rm -f ${tmpfile}
 
+# 7. now archive the artifacts or apply the git diff / review the p2diffs
