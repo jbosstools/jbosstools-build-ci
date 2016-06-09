c08aa445fba979f9b6a50a93f6bc42c9a85176d9
nickboldt <nickboldt@gmail.com>
2016-06-09 13:36:17 -0400

    can't run jbosstools and jbdevstudio TP resolutions in parallel in Jenkins, so serialize them (JBIDE-22305)

--- util/changeTargetURLs.sh
+++ util/changeTargetURLs.sh
@@ -23,11 +23,11 @@ MVN=${M2_HOME}/bin/mvn
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-tp') TARGET_PLATFORM_VERSION_MAXIMUM="$2"; shift 1;; 
-	'-p2diff') p2diff="$2"; shift 1;; # /path/to/p2diff executable
-	'-b') whichBuild="$2"; shift 1;; # could be lastBuild, lastCompletedBuild, lastSuccessfulBuild, or a build by number, eg., 38
-	'-WORKSPACE') WORKSPACE="$2"; shift 1;;
-	'-mh') M2_HOME="$2"; shift 1;;
-	'-m') MVN="$2"; shift 1;;
+  '-p2diff') p2diff="$2"; shift 1;; # /path/to/p2diff executable
+  '-b') whichBuild="$2"; shift 1;; # could be lastBuild, lastCompletedBuild, lastSuccessfulBuild, or a build by number, eg., 38
+  '-WORKSPACE') WORKSPACE="$2"; shift 1;;
+  '-mh') M2_HOME="$2"; shift 1;;
+  '-m') MVN="$2"; shift 1;;
     *) OTHER="${OTHER} $1"; shift 0;;
   esac
   shift 1
@@ -41,38 +41,33 @@ tmpfile=${WORKSPACE}/jbosstoolstargetplatformrequirements-mirror-matrix-descript
 descriptionURL=http://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/DevStudio/view/DevStudio_Master/job/jbosstoolstargetplatformrequirements-mirror-matrix/${whichBuild}/api/xml?xpath=//description
 curl -s ${descriptionURL} > ${tmpfile}
 if [[ ! $(cat ${tmpfile} | grep "http://download.jboss.org/jbosstools/updates/requirements/") ]]; then
-	echo "Error: could not parse description from ${descriptionURL}"
-	exit 1
+  echo "Error: could not parse description from ${descriptionURL}"
+  exit 1
 fi
 
 URLs=$(cat ${tmpfile} | tr " " "\n" | grep href | sed "s%href=\"\(.\+\)\#.\+\".\+%\1%")
 
 # 2. for all *.target files, find and replace similar URLs
 for u in ${URLs}; do
-	REQ_NAME=${u##http://download.jboss.org/jbosstools/updates/requirements/}; REQ_NAME=${REQ_NAME%%/*}; # echo $REQ_NAME
-	for t in $(find . -name "*.target" | grep -v "/target/"); do
-		echo "[INFO] Processing $t (${REQ_NAME}) ..."
-		sed -i "s#<repository location=\"http://download.jboss.org/jbosstools/updates/requirements/${REQ_NAME}/.\+\"/>#<repository location=\"${u}\"/>#" $t
-	done
+  REQ_NAME=${u##http://download.jboss.org/jbosstools/updates/requirements/}; REQ_NAME=${REQ_NAME%%/*}; # echo $REQ_NAME
+  for t in $(find . -name "*.target" | grep -v "/target/"); do
+    echo "[INFO] Processing $t (${REQ_NAME}) ..."
+    sed -i "s#<repository location=\"http://download.jboss.org/jbosstools/updates/requirements/${REQ_NAME}/.\+\"/>#<repository location=\"${u}\"/>#" $t
+  done
 done
 
-# 3. run verifyTarget.sh from same util/ folder 
 for d in jbosstools jbdevstudio; do
-  ${0/changeTargetURLs.sh/verifyTarget.sh} -x -b `pwd` -p $d -m ${MVN} -mrl ${WORKSPACE}/.repository &
-done
-wait
-
-# 4. generate p2diffs
-for d in jbosstools jbdevstudio; do
+  # 3. run verifyTarget.sh from same util/ folder 
+  ${0/changeTargetURLs.sh/verifyTarget.sh} -x -b `pwd` -p $d -m ${MVN} -mrl ${WORKSPACE}/.repository
+  
+  # 4. generate p2diffs
   prefix=http://download.jboss.org/jbosstools; if [[ $d == "jbdevstudio" ]]; then prefix="https://devstudio.jboss.com"; fi
   p2diffcmd="${p2diff} ${prefix}/targetplatforms/${d}target/${TARGET_PLATFORM_VERSION_MAXIMUM}/REPO/ file://"$(pwd)"/${d}/multiple/target/${d}-multiple.target.repo/"
   ${p2diffcmd} | tee ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt
   if [[ ${0/changeTargetURLs.sh/} != $0 ]]; then p2diffcheck=${0/changeTargetURLs.sh/p2diff-check.sh}; else p2diffcheck=~/tru/buildci/util/p2diff-check.sh; fi
   ${p2diffcheck} ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt | tee ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_summary_latest.txt
-done
 
-echo ""
-for d in jbosstools jbdevstudio; do
+  echo ""
   echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_latest.txt"
   echo "p2diff files: ${WORKSPACE}/p2diff_${d}_${TARGET_PLATFORM_VERSION_MAXIMUM}_summary_latest.txt"
 done
