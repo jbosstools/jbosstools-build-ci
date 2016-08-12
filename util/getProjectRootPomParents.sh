afd454bce14fd2cdd97f6e4d6f7514d0af0d9c67
nickboldt <nickboldt@gmail.com>
2016-08-12 14:39:20 -0400

    refactoring for consistency

--- util/getProjectRootPomParents.sh
+++ util/getProjectRootPomParents.sh
@@ -16,6 +16,8 @@ usage ()
     echo ""
     echo "Example 2: $0 -pv 4.4.0.Final-SNAPSHOT -skipupdate -w1 /home/nboldt/tru -w2 /home/nboldt/truu"
     echo ""
+    echo "Example 3: $0 -b master -pv 4.4.1.Alpah1-SNAPSHOT -w1 \${WORKSPACE}/jbosstools.github -w2 \${WORKSPACE}/jbdevstudio.github"
+    echo ""
     exit 1;
 }
 
@@ -24,10 +26,8 @@ if [[ $# -lt 1 ]]; then
 fi
 
 doGitUpdate=true
-parent=4.4.0.Final-SNAPSHOT # or 4.4.0.Final-SNAPSHOT
-branch=jbosstools-4.4.x # or master
-stream_jbt=4.4.neon  # or master
-stream_ds=10.0.neon # or master 
+version_parent=4.4.1.Alpha1-SNAPSHOT # or 4.4.0.Final-SNAPSHOT
+github_branch=jbosstools-4.4.x # or master
 
 WORKSPACE1=${HOME}/tru
 WORKSPACE2=${HOME}/truu
@@ -37,24 +37,29 @@ PROJECTS3="product"
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    '-b') branch="$2"; shift 1;;
-    '-pv') parent="$2"; shift 1;;
+    '-b') github_branch="$2"; shift 1;;
+    '-pv') version_parent="$2"; shift 1;;
     '-skipupdate'|'-k') doGitUpdate=false; shift 0;;
     '-w1') WORKSPACE1="$2"; shift 1;;
     '-w2') WORKSPACE2="$2"; shift 1;;
     '-p1') PROJECTS1="$2"; shift 1;; # jbosstools-* projects
     '-p2') PROJECTS2="$2"; shift 1;; # jbosstools-build-* projects
     '-p3') PROJECTS3="$2"; shift 1;; # jbdevstudio-* projects
+    '-sj') stream_jbt="$2"; shift 1;;
+    '-sd') stream_ds="$2"; shift 1;;
   esac
   shift 1
 done
 
-if [[ $branch == "master" ]]; then
-  stream_jbt="master"
-  stream_ds="master"
-elif [[ ${branch/4.4/} != ${branch} ]]; then
-  stream_jbt="4.4.neon"
-  stream_ds="10.0.neon"
+# backups if not set above
+if [[ ! ${stream_jbt} ]] || [[ ! ${stream_ds} ]]; then 
+  if [[ $github_branch == "master" ]]; then
+    stream_jbt="master"
+    stream_ds="master"
+  elif [[ ${github_branch/4.4/} != ${github_branch} ]]; then
+    stream_jbt="4.4.neon"
+    stream_ds="10.0.neon"
+  fi
 fi
 
 # TODO parameterize these?
@@ -62,14 +67,14 @@ logfile=/tmp/getProjectRootPomParents.log.txt
 errfile=/tmp/getProjectRootPomParents.err.txt
 
 gitUpdate () {
-  branch=$1
+  github_branch=$1
   if [[ ${doGitUpdate} != "false" ]]; then
     git stash; 
     git checkout -- .; git reset HEAD .
     git checkout -- .; git reset HEAD .
     git checkout master; git pull --rebase origin master -p; git rebase --abort 
     git pull origin
-    git checkout ${branch}; git pull origin ${branch}
+    git checkout ${github_branch}; git pull origin ${github_branch}
   fi
 }
 
@@ -90,15 +95,15 @@ checkProjects () {
     fi
     if [[ ${doGitUpdate} != "false" ]]; then echo "== ${j} =="; fi
     pushd ${workspace}/${prefix}${j} >/dev/null
-    gitUpdate ${branch}
+    gitUpdate ${github_branch}
     thisparent=`cat ${pomfile} | sed "s/[\r\n\$\^\t\ ]\+//g" | grep -A2 -B2 ">parent<"` # contains actual version
-    isCorrectVersion=`cat ${pomfile} | sed "s/[\r\n\$\^\t\ ]\+//g" | grep -A2 -B2 ">parent<" | grep $parent` # empty string if wrong version
+    isCorrectVersion=`cat ${pomfile} | sed "s/[\r\n\$\^\t\ ]\+//g" | grep -A2 -B2 ">parent<" | grep ${version_parent}` # empty string if wrong version
     #echo "thisparent = [$thisparent]"
     #echo "isCorrectVersion = [$isCorrectVersion]"
     if [[ ! $isCorrectVersion ]]; then
       echo -n "$j :: " >> $errfile
       # https://github.com/jbosstools/jbosstools-aerogear/commits/jbosstools-4.2.x
-      reposToCheck="${reposToCheck} https://github.com/${g_project_prefix}${j}/commits/${branch}"
+      reposToCheck="${reposToCheck} https://github.com/${g_project_prefix}${j}/commits/${github_branch}"
       jobsToCheck="${jobsToCheck} https://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/${jobname_prefix}${j}_${stream}/build"
       echo $thisparent | grep version >> $errfile
     else
@@ -123,7 +128,7 @@ cat $logfile
 echo ""
 
 if [[ ${reposToCheck} ]]; then
-  echo "Run the following to check Github for new commits on the ${branch} branch:"
+  echo "Run the following to check Github for new commits on the ${github_branch} github_branch:"
   echo ""
   echo "firefox${reposToCheck}"
   echo ""
