cbed7bfad00ec4bc1a6a6b27fb8f5200c9fcc4d2
nickboldt <nboldt@redhat.com>
2017-01-06 15:11:45 -0500

    add https:// prefix into jenkinsURL, so we can use http:// for wonka
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- util/jenkinsPost.sh
+++ util/jenkinsPost.sh
@@ -7,14 +7,14 @@ usage ()
 	echo "Usage: $0 -j JOBNAME -t TaskOrActionToPerform {-u jenkinsUser} {-p jenkinsPass} {-s jenkinsURL} {-d querystring data foo=bar&baz=foo&...}"
 	echo ""
 	echo "Example:    export userpass=\"KERBUSER:KERBPWD\" && $0 -j jbosstools-base_master -t buildWithParameters"
-	echo "Example:    $0 -j jbosstools-build.parent_master -t build -u KERBUSER -p KERBPWD -s jenkins.mw.lab.eng.bos.redhat.com/hudson/job"
+	echo "Example:    $0 -j jbosstools-build.parent_master -t build -u KERBUSER -p KERBPWD -s https://jenkins.mw.lab.eng.bos.redhat.com/hudson/job"
 	echo ""
 	exit 1
 }
 
 if [[ $# -lt 2 ]]; then usage; fi
 
-jenkinsURL="jenkins.mw.lab.eng.bos.redhat.com/hudson/job"
+jenkinsURL="https://jenkins.mw.lab.eng.bos.redhat.com/hudson/job"
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
@@ -35,18 +35,18 @@ fi
 if [[ ${userpass} = ":" ]] || [[ ! ${job} ]] || [[ ! ${task} ]]; then usage; fi
 
 echo -n "["
-prevJob=$(curl -s http://${jenkinsURL}/${job}/api/xml?xpath=//lastBuild/number | sed "s#<number>\([0-9]\+\)</number>#\1#")
-echo "${prevJob}] POST: https://${jenkinsURL}/${job}/${task} $data"
+prevJob=$(curl -s ${jenkinsURL}/${job}/api/xml?xpath=//lastBuild/number | sed "s#<number>\([0-9]\+\)</number>#\1#")
+echo "${prevJob}] POST: ${jenkinsURL}/${job}/${task} $data"
 curl -k -X POST -u ${userpass} ${data} https://${jenkinsURL}/${job}/${task}
 sleep 10s
 
 browser=/usr/bin/google-chrome; if [[ ! -x ${browser} ]]; then browser=/usr/bin/firefox; fi
 
 if [[ $task == "build"* ]]; then # build or buildWithParameters
-	nextJob=$(curl -s http://${jenkinsURL}/${job}/api/xml?xpath=//lastBuild/number | sed "s#<number>\([0-9]\+\)</number>#\1#")
+	nextJob=$(curl -s ${jenkinsURL/https/http}/${job}/api/xml?xpath=//lastBuild/number | sed "s#<number>\([0-9]\+\)</number>#\1#")
 	if [[ $prevJob != $nextJob ]]; then 
-		echo "[${nextJob}]  GET:  http://${jenkinsURL}/${job}/lastBuild/"
-		${browser} ttp://${jenkinsURL}/${job}/lastBuild/parameters http://${jenkinsURL}/${job}/lastBuild/console >/dev/null 2>/dev/null
+		echo "[${nextJob}]  GET:  ${jenkinsURL/https/http}/${job}/lastBuild/"
+		${browser} ${jenkinsURL/https/http}/${job}/lastBuild/parameters ${jenkinsURL/https/http}/${job}/lastBuild/console >/dev/null 2>/dev/null
 	fi
 fi
 
