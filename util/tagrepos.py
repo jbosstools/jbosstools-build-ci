7fffdef0214ff58dcbc90b244f57c90101daa13e
nickboldt <nboldt@redhat.com>
2017-09-19 20:34:38 -0400

    support notion of 'export userpass=ghuser:ghpwd' when running the script so it's easier to automate
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- util/tagrepos.py
+++ util/tagrepos.py
@@ -2,24 +2,40 @@
 # sudo pip install pygithub
 # 
 from github import Github, GithubException
-import sys
-import csv
+from optparse import OptionParser
+import sys, csv, os
 
-if len(sys.argv) <> 3:
-	print "Please specify both username and github password."
-	print "Usage: cat tags.csv | tagrepos.py <username> <password>"
-	print "Takes CSV formatted stream as input following the format: "
-	print "github-reponame, sha1, tag"
-	print "Example:"
-	print "jbosstools/jbosstools-hibernate, a1f287bf825a891901b41739f6a4bbaafc70bcdd, jbosstools-4.2.1.Alpha23"
-	print
-	print "Useful when combined with buildinfo.json: "
-	print "cat buildinfo.json | python buildinfo2tags.py -n <tagname> | python tagrepos.py <taggeruser> <secret>" 
-	sys.exit(-1)
+usage = "Usage: \\ \n\
+   cat buildinfo.json | python buildinfo2tags.py -n <tagname> | \\ \n\
+   python tagrepos.py --ghuser <github user> --ghpwd <github pwd> \n\
+\n\
+NOTE: rather than passing in --ghuser and --ghpwd, you can `export userpass=ghuser:ghpwd`, \n\
+and this script will read those values from the shell\n\
+\n\
+This script takes CSV formatted stream as input following the format: \n\
+github-reponame, sha1, tag\n\
+Example: jbosstools/jbosstools-hibernate, a1f287bf825a891901b41739f6a4bbaafc70bcdd, jbosstools-4.2.1.Alpha23" 
+parser = OptionParser(usage)
+parser.add_option("--ghuser", dest="ghuser",   help="github Username")
+parser.add_option("--ghpwd",  dest="ghpwd",    help="github Password")
+# NOTE: rather than passing in two flags here, you can `export userpass=ghuser:ghpwd`, 
+# and this script will read those values from the shell
 
+(options, args) = parser.parse_args()
+
+if (not options.ghuser or not options.ghpwd) and "userpass" in os.environ:
+	# check if os.environ["userpass"] is set and use that if defined
+	#sys.exit("Got os.environ[userpass] = " + os.environ["userpass"])
+	userpass_bits = os.environ["userpass"].split(":")
+	options.ghuser = userpass_bits[0]
+	options.ghpwd = userpass_bits[1]
+
+if not options.ghuser or not options.ghpwd:
+	parser.error("Must specify ALL required commandline flags")
+	
 repos = csv.reader(sys.stdin)
 		  
-g = Github(sys.argv[1], sys.argv[2])
+g = Github(options.ghuser, options.ghpwd)
 
 for counter, row in enumerate(repos):
 	
