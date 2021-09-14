e78273bb79053972da15ddb3af97c2715605a9ce
Stephane Bouchet <sbouchet@redhat.com>
2021-09-14 17:10:16 +0200

    JBIDE-27690: update mirror script (#202)
    * JBIDE-27690: added mirror util
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * added host check
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>
    
    * JBIDE-27690: fixed mirror script
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- publish/mirror.sh
+++ publish/mirror.sh
@@ -5,44 +5,12 @@
 # allow invoking this job to do nothing (if called from upstream job)
 if [[ ${PUBLISH_PATH} != "DO_NOTHING" ]]; then 
 
-  # Jenkins-specific variables
-  # where to store downloaded Eclipse zips
-  ECLIPSEDIR=/home/hudson/static_build_env/jbds/tools/sources
   # path to JDK
-  JDK_HOME=${NATIVE_TOOLS}${SEP}jdk11_last
+  JDK_HOME=${NATIVE_TOOLS}${SEP}${JAVA11}
   # shorthand for rsync
   RSYNC="rsync -arzq --protocol=28"
 
   WORKDIR=${WORKSPACE}/updates/requirements/${REQ_NAME}; mkdir -p ${WORKDIR}/; cd ${WORKDIR}
-
-  # get the build scripts
-  ${RSYNC} --delete ${WORKSPACE}/sources/jbosstools/updates/requirements/* ${WORKSPACE}/updates/requirements/
-
-  # by default, don't ignore errors (unless Jenkins job explicitly asks for it)
-  ignoreErrors=false
-
-  # if running on 32-bit slave
-  if [[ ! `uname -a | grep 64` ]]; then ECLIPSE=${ECLIPSE/-x86_64/}; fi
-
-  # get https://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/SR1/eclipse-jee-luna-SR1-linux-gtk-x86_64.tar.gz
-  # if plugins folder doesn't exist, unpack Eclipse to ${WORKSPACE}/eclipse
-  if [[ ! -d ${WORKSPACE}/eclipse/plugins/ ]]; then
-    pushd ${WORKSPACE}
-      # if we don't have this Eclipse, get it
-      echo "wget ${ECLIPSE} ..."
-      if [[ ! -f ${ECLIPSEDIR}/${ECLIPSE##*/} ]]; then wget -nc ${ECLIPSE} -O ${ECLIPSEDIR}/${ECLIPSE##*/}; fi
-      # then unpack it
-      echo "Unpack ${ECLIPSEDIR}/${ECLIPSE##*/} ..."
-      tar xzf ${ECLIPSEDIR}/${ECLIPSE##*/}
-    popd
-  fi
-
-  # put a copy of ant-contrib.jar in ${WORKDIR}/..
-  if [[ $(grep ant-contrib ${SCRIPTNAME}) ]]; then
-    M2_HOME=/qa/tools/opt/apache-maven-3.2.5/
-    $M2_HOME/bin/mvn -B dependency:copy -DtrimVersion=true -Dmdep.stripClassifier=true -Dmdep.stripVersion=true \
-      -DoutputDirectory=${WORKSPACE}/updates/requirements/ -Dartifact=ant-contrib:ant-contrib:1.0b3:jar
-  fi
 
   mkdir -p ${WORKSPACE}/tmp
   logFile=${WORKSPACE}/tmp/mirror.log.txt
@@ -50,11 +18,8 @@ if [[ ${PUBLISH_PATH} != "DO_NOTHING" ]]; then
   rm -f ${logFile} ${errFile}
 
   # get the mirror
-  if [[ ${SOURCE_URL} ]]; then SOURCE_URL_PARAM="-DURL=${SOURCE_URL}"; else SOURCE_URL_PARAM=""; fi
-  date; time ${JDK_HOME}/bin/java -cp ${WORKSPACE}/eclipse/plugins/org.eclipse.equinox.launcher_*.jar \
-      org.eclipse.equinox.launcher.Main -consoleLog -nosplash -data ${WORKSPACE}/tmp \
-      -application org.eclipse.ant.core.antRunner -f ${SCRIPTNAME} -Dversion=${VERSION} ${SOURCE_URL_PARAM} ${TASK} \
-      -DignoreErrors=${ignoreErrors} -vmargs -Declipse.p2.mirrors=false | tee ${logFile}
+  if [[ ${SOURCE_URL} ]]; then SOURCE_URL_PARAM="-DSRC_URL=${SOURCE_URL}"; else SOURCE_URL_PARAM=""; fi
+  date; time $M2_HOME/bin/mvn clean package -B -f ${WORKSPACE}/sources/mirror/pom.xml ${SOURCE_URL_PARAM} -DTARGET=${WORKDIR}/${VERSION} | tee ${logFile}
 
   if [[ -f ${logFile} ]]; then 
     echo "[INFO] Log file: ${logFile}"
@@ -85,12 +50,6 @@ if [[ ${PUBLISH_PATH} != "DO_NOTHING" ]]; then
   DESTINATION="tools@filemgmt.jboss.org:/downloads_htdocs/tools"
   date
 
-  # deprecated: only for non-requirements releases 
-  # if [[ ${PUBLISH_PATH} != "requirements" ]]; then
-  #   ${RSYNC} --delete ${WORKDIR}/${VERSION} ${DESTINATION}/builds/staging/${JOB_NAME}_${REQ_NAME}/
-  #   ${RSYNC} ${WORKDIR}/${SCRIPTNAME} ${DESTINATION}/builds/staging/${JOB_NAME}_${REQ_NAME}/
-  # fi
-
   # optionally, publish to updates/requirements/${REQ_NAME}/ too
   if [[ ${VERSION} != "SNAPSHOT" ]]; then
     date
@@ -111,7 +70,6 @@ if [[ ${PUBLISH_PATH} != "DO_NOTHING" ]]; then
 
   date
   # cleanup
-  rm -fr ${WORKSPACE}/eclipse
   rm -fr ${WORKSPACE}/tmp
 
 fi
