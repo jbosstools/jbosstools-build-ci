fc97d86d672e884bb961529f499ceb0752da4f0b
Stephane Bouchet <sbouchet@redhat.com>
2023-10-26 18:16:33 +0200

    JBIDE-28423: migrate rsync scripts to sftp
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- publish/stage.sh
+++ publish/stage.sh
@@ -202,13 +202,13 @@ for site in ${sites}; do
           echo "mkdir updates" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/ &>${consoleDest}
           echo "mkdir ${sitename}" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/ &>${consoleDest}
         fi
-        echo -e "put ${y}" | sftp -Cpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${ZIPPREFIX}${version}${suffix}.zip &>${consoleDest}
+        echo -e "put ${y}" | sftp -Cpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/ &>${consoleDest}
         # create sha256 sum if not exists
         if [[ ! -f ${y}.sha256 ]]; then
           echo "[WARN] [$site] Create updates/${sitename}/${ZIPPREFIX}${version}${suffix}.zip.sha256"
           for s in $(sha256sum ${y}); do if [[ ${s} != ${y} ]]; then echo ${s} > ${y}.sha256; fi; done          
         fi
-        echo -e "put ${y}.sha256" | sftp -Cpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${ZIPPREFIX}${version}${suffix}.zip.sha256 &>${consoleDest}
+        echo -e "put ${y}.sha256" | sftp -Cpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/ &>${consoleDest}
       elif [[ ${requireUpdateZip} -gt 0 ]]; then
         echo "[ERROR] [$site] No update site zip (repository.zip or ${ZIPPREFIX}*${suffix}.zip) found to publish in ${tmpdir}/all/ to ${DESTINATION}/${DESTDIR}/${DESTTYPE}/updates/${sitename}" | egrep "${grepstring}"
       elif [[ ${skipUpdateZip} -lt 1 ]]; then
@@ -230,9 +230,12 @@ for site in ${sites}; do
           echo "mkdir ${DESTTYPE}" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/ &>${consoleDest}
           echo "mkdir updates" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/ &>${consoleDest}
           echo "mkdir ${sitename}" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/ &>${consoleDest}
+          echo "mkdir ${version}" | sftp tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename} &>${consoleDest}
         fi
         log "[DEBUG] [$site] + sftp ${tmpdir}/all/repo/* ${DESTINATION}/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${version}/" | egrep "${grepstring}"
-        echo -e "put ${tmpdir}/all/repo/*" | sftp -Cpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${version}/ &>${consoleDest}
+        pushd ${tmpdir}/
+        echo -e "put ./all/repo/*" | sftp -Cqpr tools@filemgmt.jboss.org:/downloads_htdocs/tools/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${version}/ &>${consoleDest}
+        popd
         DEST_URLs="${DEST_URLs} ${DEST_URL}/${DESTDIR}/${DESTTYPE}/updates/${sitename}/${version}/"
       else
         # don't warn for discovery sites and browsersim standalone since they don't have update sites
