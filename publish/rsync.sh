830cc1d7b134981e8301d33c7c9022913b95760c
Stephane Bouchet <sbouchet@redhat.com>
2023-10-26 12:48:03 +0200

    JBIDE-28423: migrate rsync scripts to sftp
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- publish/rsync.sh
+++ publish/rsync.sh
@@ -98,13 +98,13 @@ if [[ ${BUILD_NUMBER} ]]; then
 	if [[ ${BUILD_TIMESTAMP} ]] && [[ ${TARGET_PATH/${BUILD_TIMESTAMP}-B${BUILD_NUMBER}} != ${TARGET_PATH} ]]; then
 		echo "[DEBUG] Symlink[BT] ${DESTINATION}/${PARENT_PATH}/latest -> ${BUILD_TIMESTAMP}-B${BUILD_NUMBER}"
 		mkdir -p $tmpdir
-		pushd $tmpdir >/dev/null; sftp -Cpq $DESTINATION/${TARGET_PATH}/; ln -s ${BUILD_TIMESTAMP}-B${BUILD_NUMBER} latest; popd >/dev/null
+		pushd $tmpdir >/dev/null; echo -e "ln -s ${BUILD_TIMESTAMP}-B${BUILD_NUMBER} latest" | sftp -Cpq $DESTINATION/${TARGET_PATH}/; popd >/dev/null
 	else
 		BUILD_DIR=$(echo ${TARGET_PATH#${PARENT_PATH}/} | sed -e "s#/\?all/repo/\?##" -e "s#/\?all/\?##")
 		if [[ ${BUILD_DIR} ]] && [[ ${BUILD_DIR%B${BUILD_NUMBER}} != ${BUILD_DIR} ]] && [[ ${TARGET_PATH/${BUILD_DIR}} != ${TARGET_PATH} ]]; then
 			echo "[DEBUG] Symlink[BD] ${DESTINATION}/${PARENT_PATH}/latest -> ${BUILD_DIR}"
 			mkdir -p $tmpdir
-			pushd $tmpdir >/dev/null; sftp -Cpq $DESTINATION/${TARGET_PATH}/; ln -s ${BUILD_DIR} latest; popd >/dev/null
+			pushd $tmpdir >/dev/null; echo -e "ln -s ${BUILD_DIR} latest" | sftp -Cpq $DESTINATION/${TARGET_PATH}/ ; popd >/dev/null
 		fi	
 	fi
 else
